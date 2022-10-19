f7414807eeb87e84c6169933b53a30a1af29617f
wilsb8 <WAFerguson1@student.fullsail.edu>
2022-10-19 13:45:52 -0700

    initial

---
+++ app/app.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = require("../api/routes/routes");
+const app = express();
+require('dotenv').config();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use("/", router); // this goes above the middleware. i might want to remember that
+                      // next time, kind of seems important.
+
+// handle CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
+    }
+    next();
+});
+
+// error handling bad urls
+app.use((req, res, next) => {
+    const error = new Error('Not found!!!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status,
+        },
+    });
+});
+
+mongoose.connect(process.env.db_url, (err) => {
+    if (err) {
+      console.error("Error", err.message);
+    } else {
+      console.log("MongoDB connected successfully.");
+    }
+});
+
+
+
+
+
+// connect database
+
+
+module.exports = app;
